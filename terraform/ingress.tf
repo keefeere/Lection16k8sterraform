eb965e0388150d203df05b4ce1ddaf32df91ea73
keefeere <keefeere@ukr.net>
2021-06-01 17:29:33 +0300

    Some HAProxy tuning

--- terraform/ingress.tf
+++ terraform/ingress.tf
@@ -2,32 +2,33 @@ resource "kubernetes_ingress" "example_ingress" {
   metadata {
     name = "example-ingress"
     annotations = {
-      "cert-manager.io/cluster-issuer" = "letsencrypt-staging" #change to letsencrypt-prod after testing
+      "cert-manager.io/cluster-issuer" = "letsencrypt-prod" #change to letsencrypt-prod after testing
       "kubernetes.io/ingress.class" = "traefik-cert-manager"
       "certmanager.k8s.io/acme-challenge-type" = "http01"
      }
   }
 
   spec {
-    backend {
-      service_name = "nginx-alpine-service"
-      service_port = 80
-    }
-
-    # rule {
-    #   http {
-    #     path {
-    #       backend {
-    #         service_name = "nginx-alpine-service"
-    #         service_port = 80
-    #       }
-
-    #       path = "/*"
-    #     }
-
-        
-    #   }
+    # backend {
+    #   service_name = "nginx-alpine-service"
+    #   service_port = 80
     # }
+
+    rule {
+      host = "keefeere.tk"
+
+      http {
+        path {
+          backend {
+            service_name = "nginx-alpine-service"
+            service_port = 80
+          }
+
+          path = "/"
+        }
+
+      }
+    }
 
      tls {
         hosts = [ "keefeere.tk" ]
